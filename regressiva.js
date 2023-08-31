4c979b579fe9dc3039c5543f3152b357c7bb44a8
pamelasanches <124888156+pamelasanches@users.noreply.github.com>
2023-08-31 09:13:18 -0300

    Add files via upload

---
+++ regressiva.js
@@ -0,0 +1,11 @@
+tempo = 5000;
+const idade = 16;
+age = 16;
+age = age +1
+var timer =setInterval(chamar,tempo);
+
+function chamar(){
+//document.getElement só é chamado com tempo
+document.getElementById("mostrar").innerHTML = idade;
+document.getElementById("exibir").innerHTML = age;
+}
